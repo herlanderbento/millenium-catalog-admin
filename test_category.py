cd4a2f83dedcff8c6ed24deaa3da461a955a857e
herlanderbento <herlanderbento19@gmail.com>
2024-08-14 18:10:53 +0100

    feat: :construction: first commit

---
+++ test_category.py
@@ -0,0 +1,15 @@
+import unittest
+
+from category import Category
+
+class TestCategory(unittest.TestCase):
+  def test_name_is_required(self):
+    with self.assertRaisesRegex(TypeError, "missing 1 required positional argument: 'name'"):
+      Category()
+  
+  def test_name_must_have_less_than_255_characters(self):
+    with self.assertRaisesRegex(ValueError, "Name must be less than 255 characters"):
+      Category(name="a" * 256)
+
+if __name__ == "__main__":
+  unittest.main()

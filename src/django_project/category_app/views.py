37ae5039af0b6ae492c1067a8aa14478b6b29350
Herlander Bento <herlanderbento19@gmail.com>
2024-08-22 17:43:37 +0100

    feat: :construction: feat(category): Implementing our API - pt 2

--- src/django_project/category_app/views.py
+++ src/django_project/category_app/views.py
@@ -1,9 +1,20 @@
+from uuid import UUID
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK
+from rest_framework.status import HTTP_200_OK, HTTP_404_NOT_FOUND
 
+from django_project.category_app.serializers import (
+    ListCategoryResponseSerializer,
+    RetrieveCategoryRequestSerializer,
+    RetrieveCategoryResponseSerializer,
+)
+from src.core.category.application.use_cases.exceptions import CategoryNotFound
+from src.core.category.application.use_cases.get_category import (
+    GetCategory,
+    GetCategoryRequest,
+)
 from django_project.category_app.repository import DjangoORMCategoryRepository
 
 from src.core.category.application.use_cases.list_category import (
@@ -15,19 +26,29 @@ from src.core.category.application.use_cases.list_category import (
 class CategoryViewSet(viewsets.ViewSet):
     def list(self, request: Request) -> Response:
         use_case = ListCategory(repository=DjangoORMCategoryRepository())
-        response = use_case.execute(request=ListCategoryRequest())
+        output = use_case.execute(request=ListCategoryRequest())
 
-        categories = [
-            {
-                "id": str(category.id),
-                "name": category.name,
-                "description": category.description,
-                "is_active": category.is_active,
-            }
-            for category in response.data
-        ]
+        response_serializer = ListCategoryResponseSerializer(output)
 
         return Response(
             status=HTTP_200_OK,
-            data=categories,
+            data=response_serializer.data,
+        )
+
+    def retrieve(self, request: Request, pk: None) -> Response:
+        serializer = RetrieveCategoryRequestSerializer(data={"id": pk})
+        serializer.is_valid(raise_exception=True)
+
+        input = GetCategoryRequest(**serializer.validated_data)
+        use_case = GetCategory(repository=DjangoORMCategoryRepository())
+
+        try:
+            output = use_case.execute(request=input)
+        except CategoryNotFound:
+            return Response(status=HTTP_404_NOT_FOUND)
+
+        response_serializer = RetrieveCategoryResponseSerializer(output)
+        return Response(
+            status=HTTP_200_OK,
+            data=response_serializer.data,
         )
